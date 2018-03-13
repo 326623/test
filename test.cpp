3884c5cd0231abbdc953d10361a0453f4ab4b8f2
king-kong <yangqp5@outlook.com>
2018-03-13 22:02:51 +0800

    files

---
+++ test.cpp
@@ -0,0 +1,34 @@
+
+#include <opencv2/core.hpp>
+#include <iostream>
+#include <string>
+#include <vector>
+using std::vector;
+using std::string;
+using std::cout;
+using std::endl;
+using std::cerr;
+using std::ostream;
+using namespace cv;
+
+
+
+// struct data {
+//   float a,b,c;
+// };
+
+int main(int argc, char** argv)
+{
+
+
+  try {
+//    FileStorage fs("svm_1_split_0.json", 0); // use postfix to know it's json, 0 for read
+    FileStorage fs("svm_1.json", 0);
+    FileNode root = fs[(string)"svm" + (string)"_1"];
+    cout << root.size() << std::endl;
+    cout << fs.getFormat() << std::endl;
+  }
+  catch(const Exception& e) {
+    cout << "error: " << e.what() << std::endl;
+  }
+}
