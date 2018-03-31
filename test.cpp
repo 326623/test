987ecb0a800d998dee163b697a3b25205c05bfa3
king-kong <yangqp5@outlook.com>
2018-03-31 08:04:40 +0800

    merge

--- test.cpp
+++ test.cpp
@@ -1,34 +1,101 @@
+// #include <iostream>
+// using namespace std;
 
-#include <opencv2/core.hpp>
-#include <iostream>
-#include <string>
-#include <vector>
-using std::vector;
-using std::string;
-using std::cout;
-using std::endl;
-using std::cerr;
-using std::ostream;
-using namespace cv;
-
-
-
-// struct data {
-//   float a,b,c;
+// struct aproxy {
+//   aproxy(int& r) : mPtr(&r) {}
+//   void operator = (int n) {
+//     if (n > 1) {
+//       throw "not binary digit";
+//     }
+//     *mPtr = n;
+//   }
+//   int * mPtr;
 // };
 
-int main(int argc, char** argv)
-{
+// //template <typename Any>
 
+// struct array {
+//   int mArray[10];
+//   aproxy operator[](int i) {
+//     return aproxy(mArray[i]);
+//   }
+// };
 
-  try {
-//    FileStorage fs("svm_1_split_0.json", 0); // use postfix to know it's json, 0 for read
-    FileStorage fs("svm_1.json", 0);
-    FileNode root = fs[(string)"svm" + (string)"_1"];
-    cout << root.size() << std::endl;
-    cout << fs.getFormat() << std::endl;
-  }
-  catch(const Exception& e) {
-    cout << "error: " << e.what() << std::endl;
-  }
-}
+// class Testable1 {
+//   bool ok_;
+// public:
+//   explicit Testable1(bool b=true):ok_(b) {}
+
+//   // conversion operators
+//   operator bool() const {
+//     return ok_;
+//   }
+// };
+
+// class Testable2 {
+//   bool not_ok_;
+// public:
+//   explicit Testable2(bool b=true):not_ok_(!b) {}
+//   bool operator!() const {
+//     return not_ok_;
+//   }
+// };
+
+// // operator void* version
+// class Testable3 {
+//   bool ok_;
+// public:
+//   explicit Testable3(bool b=true):ok_(b) {}
+
+//   operator void*() const {
+//     //return ok_ == true ? this : 0; this won't compile because you are returning a pointer, please don't do that...
+//     return ok_ == true ? (void*)1 : 0;
+//   }
+// };
+
+// class Testable4 {
+//   bool ok_;
+// public:
+//   explicit Testable4(bool b=true):ok_(b) {}
+
+//   class nested_class {};
+
+//   operator const nested_class*() const {
+//     return ok_ ? reinterpret_cast<const nested_class*>(this) : 0;
+//   }
+// };
+
+// class Testable {
+//   bool ok_;
+//   typedef
+// }
+
+// int main() {
+
+//   Testable1 a(false);
+//   if (a) {
+//     cout << "hi" << endl;
+//   }
+
+//   Testable3 b(true);
+//   if (void * p = b) {
+//     cout << p << endl;
+//     cout << "Testable3 works" << endl;
+//   }
+
+//   Testable4 c(true), d(false);
+//   if (c && !d) {
+//     cout << "Testable4 works" << endl;
+//   }
+
+//   cout << !!1 << endl;
+//   //cout << () << endl;
+//   return 0;
+// }
+
+// // template<typename Any>
+// // void testing(Any a) {
+// //   if(a) {
+// //     cout << 
+// //   }
+// // }
