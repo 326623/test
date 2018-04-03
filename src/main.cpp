fbf3dd820fd0abbe805f2ed15368cd1f3966e476
king-kong <yangqp5@outlook.com>
2018-04-03 22:34:02 +0800

    some experiment with gsl integral

--- src/main.cpp
+++ src/main.cpp
@@ -1,62 +1,50 @@
-// #include <boost/numeric/ublas/vector.hpp>
-// #include <boost/numeric/ublas/matrix.hpp>
-// #include <boost/numeric/ublas/io.hpp>
 
-// int main() {
-//   // It seems that namespace are in seperate scope, which makes sense
-//   {
-//     using namespace std;
-//     vector<double> v(3,1);
-//     for (int i = 0; i < v.size(); ++ i) {
-//       cout << v[i] << endl;
-//     }
-//   }
+//#include "operator_overloading.hpp"
+#include <stdio.h>
+#include <math.h>
+#include <gsl/gsl_integration.h>
 
-//   {
-//     using namespace boost::numeric::ublas;
-//     //vector<double> v (3);
-//     for (unsigned i = 0; i < 3; ++ i) {
-//       unit_vector<double> v (3, i);
-//       std::cout << v << std::endl;
-//       //v (i) = i;
-//     }
-//   }
+double f (double x, void * params) {
+  double alpha = *(double *) params;
+  double f = log(alpha*x) / sqrt(x);
+  return f;
+}
 
-//   {
-//     using namespace boost::numeric::ublas;
-//     zero_vector<double> v (3);
-//     std::cout << v << std::endl;
-//   }
+int main(void) {
+  gsl_integration_workspace * w
+    = gsl_integration_workspace_alloc (1000);
 
-//   {
-//     using namespace boost::numeric::ublas;
-//     scalar_vector<double> v (3);
-//     std::cout << v << std::endl;
-//   }
+  double result, error;
+  double expected = -4.0;
+  double alpha = 1.0;
 
-//   {
-//     using namespace boost::numeric::ublas;
-//     matrix<double> m (3, 3);
-//     //matrix<double> m (3, 3);
-//     for (unsigned i = 0; i < m.size1 (); ++ i)
-//       for (unsigned j = 0; j < m.size2 (); ++ j)
-//         m (i, j) = 3 * i + j;
-//     std::cout << m << std::endl;
-//   }
+  gsl_function F;
+  F.function = &f;
+  F.params = &alpha;
 
-// }
+  gsl_integration_qags (&F, 0, 1, 0, 1e-7, 1000,
+                        w, &result, &error);
 
-#include "operator_overloading.hpp"
+  printf ("result          = % .18f\n", result);
+  printf ("exact result    = % .18f\n", expected);
+  printf ("estimated error = % .18f\n", error);
+  printf ("actual error    = % .18f\n", result - expected);
+  printf ("intervals       = %zu\n", w->size);
 
-int main() {
-  using namespace op_overload;
-  //std::cout << "hello there" << std::endl;
-  hello_you a(1);
-  //std::cout << std::is_base_of<hello_you, hello_you>::value << std::endl;
+  gsl_integration_workspace_free (w);
 
-  //std::cout << a << std::endl;
+  int n = 10000;
+  gsl_integration_glfixed_table * w1 =
+    gsl_integration_glfixed_table_alloc(2*n-1);
 
-  operator<< (std::cout, a);// << std::endl;
+  double xi, wi;
+  for (int i = 1; i <= n; ++ i) {
+    gsl_integration_glfixed_point(-1, 1, i, &xi, &wi, w1);
+    printf ("% .10f % .10f\n", xi, wi);
+  }
 
-  std::cout << a << std::endl;
+  printf( "% .18f\n", gsl_integration_glfixed(&F, 0, 1, w1));
+
+  gsl_integration_glfixed_table_free (w1);
+  return 0;
 }
