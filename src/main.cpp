5d5e5727bfef2f71ce83a9ec9b24c12c83dba324
326623 <yangqp5@mail2.sysu.edu.cn>
2018-04-15 00:34:51 +0800

    gsl testing

--- src/main.cpp
+++ src/main.cpp
@@ -52,26 +52,45 @@
 //#include <boost/numeric/ublas/triangular.hpp>
 //#include <boost/numeric/ublas/io.hpp>
 #include <vector>
+#include <gsl/gsl_integration.h>
+#include <iostream>
+#include <cmath>
 
-int main () {
-  // using namespace boost::numeric::ublas;
-  // matrix<double> m (3, 3);
-  // vector<double> v (3);
-  // for (unsigned i = 0; i < std::min (m.size1 (), v.size ()); ++ i) {
-  //   for (unsigned j = 0; j <= i; ++ j)
-  //     m (i, j) = 3 * i + j + 1;
-  //   v (i) = i;
-  // }
-
-  // std::cout << m << std::endl;
-  // std::cout << v << std::endl;
-
-  // std::cout << solve (m, v, lower_tag ()) << std::endl;
-  // std::cout << solve (v, m, lower_tag ()) << std::endl;
-
-  //std::vector<int> i{1,2};
-  //auto j = &i[1];
-  //const int i = 0;
-  //&i = 1;
-  //&1;
+double f(double x, void * params) {
+  return std::sin(x);
 }
+
+int main() {
+  gsl_integration_glfixed_table * integral_table =
+    gsl_integration_glfixed_table_alloc(10);
+
+  gsl_function F;
+  F.function = f;
+
+  std::cout
+    << gsl_integration_glfixed(&F, 0, M_PI, integral_table) << std::endl;
+}
+
+
+// int main () {
+//   // using namespace boost::numeric::ublas;
+//   // matrix<double> m (3, 3);
+//   // vector<double> v (3);
+//   // for (unsigned i = 0; i < std::min (m.size1 (), v.size ()); ++ i) {
+//   //   for (unsigned j = 0; j <= i; ++ j)
+//   //     m (i, j) = 3 * i + j + 1;
+//   //   v (i) = i;
+//   // }
+
+//   // std::cout << m << std::endl;
+//   // std::cout << v << std::endl;
+
+//   // std::cout << solve (m, v, lower_tag ()) << std::endl;
+//   // std::cout << solve (v, m, lower_tag ()) << std::endl;
+
+//   //std::vector<int> i{1,2};
+//   //auto j = &i[1];
+//   //const int i = 0;
+//   //&i = 1;
+//   //&1;
+// }
