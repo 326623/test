e0a2efabd84840420ee02421f8adf91de70d8bc7
326623 <yangqp5@mail2.sysu.edu.cn>
2018-08-03 13:19:30 +0800

    sync

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,42 @@
-// person.hxx
-//
+// // person.hxx
+// //
 
-// person.hxx
-//
+// // person.hxx
+// //
 
-#include <string>
+// #include <string>
 
-#include <odb/core.hxx>     // (1)
+// #include <odb/core.hxx>     // (1)
 
-#pragma db object           // (2)
-class person
-{
-private:
-  person () {}              // (3)
+// #pragma db object           // (2)
+// class person
+// {
+// private:
+//   person () {}              // (3)
 
-  friend class odb::access; // (4)
+//   friend class odb::access; // (4)
 
-#pragma db id auto        // (5)
-  unsigned long id_;        // (5)
+// #pragma db id auto        // (5)
+//   unsigned long id_;        // (5)
 
-  std::string first_;
-  std::string last_;
-  unsigned short age_;
-};
+//   std::string first_;
+//   std::string last_;
+//   unsigned short age_;
+// };
+
+// no definition for the class, cause compilation error
+template <typename T>
+class TD;
+
+int x;
+int y;
+
+//TD<decltype(x)> xType;
+//TD<decltype(y)> yType;
+
+#include <iostream>
+#include <typeinfo>
+int main() {
+  std::cout << typeid(x).name() << '\n';
+  return 0;
+}
