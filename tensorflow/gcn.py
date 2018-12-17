2588bc6fd747f7411fff48f182c0bfe1ee446687
king-kong <yangqp5@outlook.com>
2018-12-17 14:23:11 +0800

    tensorflow build block mostly done, leaving debugging.

--- tensorflow/gcn.py
+++ tensorflow/gcn.py
@@ -1,37 +1,60 @@
 import tensorflow as tf
+from tqdm import tqdm
 
-class base_model(object):
+class BaseModel(object):
     def __init__(self):
-        self._regularitizers = []
+        self._regularizers = []
 
     def predict_with_labels(self, data, labels, sess=None):
-        data_init_op = self.data_iterator.make_initailizer(data)
-        labels_init_op = self.labels_iterator.make_initalizer(labels)
+        """
+        Args:
+          data: `tf.data.dataset` containing input features
+          labels: `tf.data.dataset` containing input labels
+          sess: the current `tf.Session` or None
+
+        Returns:
+          batch_labels: `numpy` ...
+          batch_loss: `numpy` ...
+          batch_prediction: `numpy` ...
+        """
+        data_init_op = self.data_iterator.make_initializer(data)
+        labels_init_op = self.labels_iterator.make_initializer(labels)
         feed_dict = {self.ph_dropout: 1.0}
         sess = self._get_session(sess)
 
         try:
             sess.run([data_init_op, labels_init_op])
             while True:
-                batch_loss, batch_prediction = self.session.run(
-                    [self.op_loss, self.op_prediction], feed_dict)
+                batch_labels, batch_loss, batch_prediction = sess.run(
+                    [self.pipe_labels, self.op_loss, self.op_prediction], feed_dict)
                 # In the event of large dataset, we may want to write prediction
                 # to save memory, the limitation is the lack of multi-thread support
                 # with the keyword yield
-                yield batch_loss, batch_prediction
+                yield batch_labels, batch_loss, batch_prediction
         except tf.errors.OutOfRangeError:
             pass
 
     def predict(self, data, sess=None):
-        data_init_op = self.data_iterator.make_initailizer(data)
+        """
+        Args:
+          data: `tf.data.dataset` containing input features
+          labels: `tf.data.dataset` containing input labels
+          sess: the current `tf.Session` or None
+
+        Returns:
+          batch_labels: `numpy` ...
+          batch_loss: `numpy` ...
+          batch_prediction: `numpy` ...
+        """
+        data_init_op = self.data_iterator.make_initializer(data)
         feed_dict = {self.ph_dropout: 1.0}
         sess = self._get_session(sess)
 
         try:
             sess.run([data_init_op])
             while True:
-                batch_prediction = self.session.run(
-                    [self.op_prediction], feed_dict)
+                batch_prediction = sess.run(
+                    [self.op_logits, self.op_prediction], feed_dict)
                 # In the event of large dataset, we may want to write prediction
                 # to save memory
                 yield batch_prediction
@@ -39,13 +62,13 @@ class base_model(object):
             pass
 
     def evaluate(self, data, labels, sess=None):
-        t_process, t_wall = time.process_time(), time.time()
-        predictions, loss = [], []
-        for batch_loss, batch_prediction in self.predict(data, labels, sess):
-            prediction.extend(batch_prediction)
-            loss.extend(batch_loss)
+        c_labels, c_predictions, c_loss = [], [], []
+        for batch_labels, batch_loss, batch_prediction in self.predict_with_labels(data, labels, sess):
+            c_labels.extend(batch_labels)
+            c_predictions.extend(batch_prediction)
+            c_loss.extend(batch_loss)
 
-        ncorrects = sum(predictions)
+        ncorrects = sum(predictions == labels)
 
     def inference(self, data, dropout):
         """
@@ -54,8 +77,14 @@ class base_model(object):
         logits = self._inference(data, dropout)
         return logits
 
-    def fit(self, training_data, training_labels, val_data, val_labels):
-        """Runs the entire epoch of the train_dataset
+    def fit(self, training_data, training_labels, val_data, val_labels, progress_per=100, total=None):
+        """
+        Runs the entire epoch of the train_dataset
+        training_data:
+        training_labels:
+        val_data:
+        val_labels:
+        progress_per:
         """
         sess = tf.Session(graph=self.graph)
         # some model saving here
@@ -65,76 +94,131 @@ class base_model(object):
         # val_data_init_op = self.data_iterator.make_initailizer(val_data)
         # val_labels_init_op = self.labels_iterator.make_initializer(val_labels)
 
-        for _ in range(10):
-            sess.run([training_data_init_op, training_labels_init_op])
-            try:
-                while True:
-                    feed_dict = {self.ph_dropout: self.dropout}
-                    # The only guarantee tensorflow makes about order of execution is that
-                    # all dependencies (either data or control) of an op are executed
-                    # before that op gets executed.
-                    loss_average, train_op = sess.run([self.op_loss_average, self.op_train, feed_dict])
-            except tf.errors.OutOfRangeError:
-                pass
+        sess.run([training_data_init_op, training_labels_init_op])
+        # try:
+        #     pbar = tqdm(total=total, mininterval=1)
+        #     while True:
+        #         feed_dict = {self.ph_dropout: self.dropout}
+        #         # The only guarantee tensorflow makes about order of execution is that
+        #         # all dependencies (either data or control) of an op are executed
+        #         # before that op gets executed.
+        #         global_step, loss, loss_average, train_op = \
+        #             sess.run([self.global_step, self.op_loss, self.op_loss_average, self.op_train], feed_dict)
+        #         pbar.update()
+        #         pbar.set_postfix(trn_loss="{:.3e}".format(loss),
+        #                          trn_loss_aver="{:.3e}".format(loss_average),
+        #                          refresh=False)
+        #         # if total:
+        #         #     tf.logging.log_every_n(tf.logging.INFO, 'step: %d', progress_per, global_step)
+        #         # tf.logging.log_every_n(tf.logging.INFO, 'loss: %f, average loss: %f',
+        #         #                        progress_per, loss, loss_average)
 
-            try:
-                while True:
-                    self.evaluate(val_data, val_labels, sess)
-                    # self._get_session().run([self.op_prediction])
+        # except tf.errors.OutOfRangeError:
+        #     pbar.close()
+        self.evaluate(val_data, val_labels, sess)
+
+    def training(self, loss, learning_rate, decay_steps=None, decay_rate=None, momentum=None):
+        with tf.name_scope('training'):
+            # learning rate
+            self.global_step = tf.Variable(0, name='global_step', trainable=False)
+            optimizer = tf.train.AdamOptimizer(learning_rate)
+            grads = optimizer.compute_gradients(loss)
+            train_op = optimizer.apply_gradients(grads, self.global_step)
+            return train_op
+
+    def probabilties(self, logits):
+        """
+        Returns the probability of a sample to belong to each class.
+        """
+        with tf.name_scope('probabilities'):
+            probabilities = tf.nn.softmax(logits)
+            return probabilities
+
+    def prediction(self, logits):
+        with tf.name_scope('prediction'):
+            prediction = tf.argmax(logits, axis=1)
+            return prediction
 
     def loss(self, logits, labels, regularization=None):
         with tf.name_scope('loss'):
             with tf.name_scope('cross_entropy'):
                 labels = tf.to_int64(labels)
-                cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels)
+                # cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels)
+                cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels)
                 cross_entropy = tf.reduce_mean(cross_entropy)
+
             with tf.name_scope('regularization'):
-                regularization *= tf.add_n(self.regularizers)
+                regularization *= tf.add_n(self._regularizers)
+                loss = cross_entropy + regularization
 
-            loss = cross_entropy + regularization
-            return loss
+            with tf.name_scope('averages'):
+                averages = tf.train.ExponentialMovingAverage(0.9)
+                op_averages = averages.apply([cross_entropy, regularization, loss])
+                # tf.summary.scalar('loss/avg/cross_entropy', averages.average(cross_entropy))
+                # tf.summary.scalar('loss/avg/regularization', averages.average(regularization))
+                # tf.summary.scalar('loss/avg/total', averages.average(loss))
+                with tf.control_dependencies([op_averages]):
+                    loss_average = tf.identity(averages.average(loss), name='control')
 
-    def training(self, loss, learning_rate, decay_steps=None, decay_rate=None, momentum=None):
-        with tf.name_scope('training'):
-            # learning rate
-            global_step = tf.Variable(0, name='global_step', trainable=False)
-            optimizer = tf.train.AdamOptimizer(learning_rate)
-            grads = optimizer.compute_gradients(loss)
-            train_op = optimizer.apply_gradients(grads, global_step)
-            return train_op
+            return loss, loss_average
 
-    def build_graph(self, data_format, labels_format):
+    #def input_pipeline(self, data_format, labels_format):
+    def build_graph(self, data_format, labels_format, graph=None):
         """
         data_format, labels_format: output_types, output_shapes of data_dataset, labels_dataset
         distinguish data and labels because some operations don't depend on labels, e.g.,
         predict, inference etc.
         """
-        self.graph = tf.Graph
+        if graph:
+            self.graph = graph
+        else:
+            self.graph = tf.Graph()
 
         with self.graph.as_default():
 
             # Inputs
             with tf.name_scope('inputs'):
-                self.data_iterator = tf.data.Iterator.from_structure(data_format)
-                self.labels_iterator = tf.data.Iterator.from_structure(labels_format)
+                self.data_iterator = tf.data.Iterator.from_structure(*data_format)
+                self.labels_iterator = tf.data.Iterator.from_structure(*labels_format)
                 self.pipe_data = self.data_iterator.get_next()
                 self.pipe_labels = self.labels_iterator.get_next()
                 self.ph_dropout = tf.placeholder(tf.float32, (), 'dropout')
                 #self.pipe_data, self.pipe_labels, self.pipe_dropout \
                     #= input_iterator.get_next()
 
-            op_logits = self.inference(self.pipe_data, self.ph_dropout)
-            # self.op_loss, self.op_loss_average = self.loss(op_logits,
-            #                                                self.pipe_labels,
-            #                                                self.regularization)
-            self.op_loss = self.loss(op_logits, self.pipe_labels, self.regularization)
-            self.op_train = self.training(self.op_loss, self.learning_rate,
-                                          self.decay_steps, self.decay_rate,
-                                          self.momentum)
-            self.op_prediction = self.prediction(op_logits)
+            self.op_logits = self.inference(self.pipe_data, self.ph_dropout)
+            self.op_loss, self.op_loss_average = self.loss(self.op_logits,
+                                                           self.pipe_labels,
+                                                           self.regularization)
+            # self.op_loss = self.loss(self.op_logits, self.pipe_labels, self.regularization)
+            self.op_train = self.training(self.op_loss, self.learning_rate,)
+                                          #self.decay_steps, self.decay_rate,
+                                          #self.momentum)
+            self.op_prediction = self.prediction(self.op_logits)
             self.op_init = tf.global_variables_initializer()
 
             # self.op_summary = tf.summary.merge_all()
         #     self.op_saver = tf.train.Saver(max_to_keep=5)
 
         # self.graph.finalize()
+
+    def _weight_variable(self, shape, regularization=True):
+        initial = tf.truncated_normal_initializer(0, 0.1)
+        var = tf.get_variable('weights', shape, tf.float32, initializer=initial)
+        if regularization:
+            self._regularizers.append(tf.nn.l2_loss(var))
+            # tf.summary.histogram(var.op.name, var)
+        return var
+
+    def _bias_variable(self, shape, regularization=True):
+        initial = tf.constant_initializer(0.1)
+        var = tf.get_variable('bias', shape, tf.float32, initializer=initial)
+        if regularization:
+            self._regularizers.append(tf.nn.l2_loss(var))
+            # tf.summary.histogram(var.op.name, var)
+        return var
+
+    def _get_session(self, sess=None):
+        if sess is None:
+            sess = tf.Session(graph=self.graph)
+        return sess
--- tensorflow/test.py
+++ tensorflow/test.py
@@ -1,10 +1,76 @@
-import tensorflow as tf
+# import tensorflow as tf
 
-dataset = tf.data.TextLineDataset(['LSTM.py'])
-iterator = dataset.make_initializable_iterator()
-next_element = iterator.get_next()
-with tf.Session() as sess:
-    sess.run(iterator.initializer)
-    sess.run(next_element)
-    sess.run(next_element)
-    sess.run(next_element)
+# dataset = tf.data.TextLineDataset(['LSTM.py'])
+# iterator = dataset.make_initializable_iterator()
+# next_element = iterator.get_next()
+# with tf.Session() as sess:
+#     sess.run(iterator.initializer)
+#     sess.run(next_element)
+#     sess.run(next_element)
+#     sess.run(next_element)
+import numpy as np
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+from gcn import BaseModel
+mnist = input_data.read_data_sets('data/mnist', one_hot=True)
+
+train_data = tf.data.Dataset.from_tensor_slices(
+    mnist.train.images.astype(np.float32))
+val_data = tf.data.Dataset.from_tensor_slices(
+    mnist.validation.images.astype(np.float32))
+test_data = tf.data.Dataset.from_tensor_slices(
+    mnist.test.images.astype(np.float32))
+train_labels = tf.data.Dataset.from_tensor_slices(
+    mnist.train.labels)
+val_labels = tf.data.Dataset.from_tensor_slices(
+    mnist.validation.labels)
+test_labels = tf.data.Dataset.from_tensor_slices(
+    mnist.test.labels)
+
+tf.logging.set_verbosity(tf.logging.INFO)
+class CNN(BaseModel):
+    def __init__(self, shape_size, channel_size,
+                 feature_size, n_classes):
+        super().__init__()
+        # input grid: K x K, channel: C,
+        # output channel: F
+        self._K = shape_size
+        self._C = channel_size
+        self._F = feature_size
+        self._N = n_classes
+        self.learning_rate = 0.0001
+        self.dropout = 0.5
+        self.regularization = 0.001
+
+    def _inference(self, x, dropout):
+        with tf.variable_scope('conv1'):
+            W = self._weight_variable([self._K, self._K, 1, self._F])
+            b = self._bias_variable([self._F])
+
+            x_2d = tf.reshape(x, [-1, self._K, self._K, self._C])
+            y_2d = tf.nn.conv2d(x_2d, W, strides=[1, 1, 1, 1], padding='SAME') + b
+            y_2d = tf.nn.relu(y_2d)
+
+        with tf.variable_scope('fc1'):
+            y = tf.reshape(y_2d, [-1, self._K * self._K * self._F])
+            W = self._weight_variable([self._K * self._K * self._F, self._N])
+            b = self._bias_variable([self._N])
+            y = tf.matmul(y, W) + b
+
+        return y
+
+cnn = CNN(28, 1, 2, 10)
+train_data = train_data.batch(200).prefetch(6)
+train_labels = train_labels.batch(200).prefetch(6)
+val_data = val_data.batch(200).prefetch(6)
+val_labels = val_labels.batch(200).prefetch(6)
+data_format = (train_data.output_types, train_data.output_shapes)
+labels_format = (train_labels.output_types, train_labels.output_shapes)
+cnn.build_graph(data_format, labels_format, tf.get_default_graph())
+cnn.fit(train_data, train_labels, val_data, val_labels)
+# iterator = dataset.make_one_shot_iterator()
+# next_batch = iterator.get_next()
+
+# sess = tf.Session()
+# for _ in range(1):
+#     print(sess.run(next_batch).reshape(28, 28))
