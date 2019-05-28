531e2d86b566841400db5c76b4a5b48a7097291b
king-kong <yangqp5@outlook.com>
2019-05-28 22:41:11 +0800

    finished with reduction, continue to optimize it

--- cuda/reduction.cu
+++ cuda/reduction.cu
@@ -1,7 +1,11 @@
 #include <glog/logging.h>
+
 #include <random>
+#include <iostream>
 
 #include <curand.h>
+
+// Compile with nvcc reduction.cu -lcurand -lglog -lgflags
 
 // __global__ void RandomArray(float* array, int n) {
 
@@ -9,19 +13,56 @@
 
 __global__ void FindMax(const float* array, int n,
                         // output parameter
-                        int* max_index) {
-  int index = 
+                        float* out_array) {
+  // What happens if the shared memory is not enough? Runtime error?
+  extern __shared__ float shared_array[];
+  // First, for each thread of each block, look for max
+  int thread_id = threadIdx.x;
+  int id = blockIdx.x * blockDim.x + threadIdx.x;
+  int stride = blockDim.x * gridDim.x;
+
+  float local_max = array[id];
+  // Find max for each thread
+  for (int i = id + stride; i < n; i += stride) {
+    if (local_max < array[i]) {
+      local_max = array[i];
+    }
+  }
+
+  // Each thread loads its max into each block's shared mem
+  shared_array[thread_id] = local_max;
+  __syncthreads();
+
+  // Perform reduction upon each block
+  for (int num_workers = blockDim.x / 2, step_size = 1; num_workers > 0;
+       num_workers /= 2, step_size *= 2) {
+    if (thread_id < num_workers) {
+      int index = 2 * step_size * thread_id;
+      float num1 = shared_array[index];
+      float num2 = shared_array[index + step_size];
+
+      if (num1 < num2)
+        shared_array[index] = num2;
+      else
+        shared_array[index] = num1;
+    }
+    __syncthreads();
+  }
+  // All blocks have its max in shared_mem[0]
+  // Write to global memory for synchronization
+  if (thread_id == 0)
+    out_array[blockIdx.x] = shared_array[0];
 }
 
-void FindMax(const float* array, int n, int* max_index) {
-  if (n <= 0) return;
-  int max = array[0];
+float HostFindMax(const float* array, int n) {
+  if (n <= 0) return -1;
+  float max = array[0];
   for (int i = 0; i < n; ++i) {
-    if (array[i] < max) {
-      *max_index = i;
+    if (array[i] > max) {
       max = array[i];
     }
   }
+  return max;
 }
 
 #define CUDA_CALL(x)                                  \
@@ -44,15 +85,12 @@ int main(int argc, char* argv[]) {
   if (argc != 2) return -1;
   int n = std::atoi(argv[1]);
   float* device_array;
-  int* device_max_index;
   float* array;
-  int* max_index;
   curandGenerator_t gen;
 
-  CUDA_CALL(cudaMalloc(device_array, n * sizeof(float)));
-  CUDA_CALL(cudaMalloc(device_max_index, sizeof(int)));
-  malloc(array, n * sizeof(float));
-  malloc(max_index, sizeof(int));
+  CUDA_CALL(cudaMalloc(&device_array, n * sizeof(float)));
+  array = new float[n];// malloc(n * sizeof(float));
+  // max_index = new int; // malloc(max_index, sizeof(int));
 
   // Random number generation
   std::random_device rd;
@@ -60,19 +98,34 @@ int main(int argc, char* argv[]) {
   CURAND_CALL(curandSetPseudoRandomGeneratorSeed(gen, rd()));
   CURAND_CALL(curandGenerateUniform(gen, device_array, n));
   CURAND_CALL(curandDestroyGenerator(gen));
+  CUDA_CALL(cudaDeviceSynchronize());
 
-  FindMax<<<>>>(device_array, n, device_max_index);
-  CUDA_CALL(CudaMemcpy(device_array, array, n * sizeof(float),
+  // Since the FindMax would alter device_array, first copy to host
+  CUDA_CALL(cudaMemcpy(array, device_array, n * sizeof(float),
                        cudaMemcpyDeviceToHost));
-  CUDA_CALL(CudaMemcpy(device_max_index, max_index, sizeof(int),
+  // for (int i = 0; i < n-1; ++i)
+  //   std::cout << array[i] << ' ';
+  // std::cout << array[n-1] << '\n';
+  float max_host = HostFindMax(array, n);
+
+  // First FindMax() would assign the local maxima to the first 64 of device
+  // array, which is the block size of the first call
+  FindMax<<<64, 64, 64 * sizeof(float)>>>(device_array, n, device_array);
+  FindMax<<<1, 64, 64 * sizeof(float)>>>(device_array, n, device_array);
+  CUDA_CALL(cudaDeviceSynchronize());
+  CUDA_CALL(cudaMemcpy(array, device_array, 1 * sizeof(float),
                        cudaMemcpyDeviceToHost));
 
-  int kernel_max_index = *max_index;
-  FindMax(array, n, max_index);
-  CHECK_EQ(*max_index, kernel_max_index);
+  float max_device = array[0];
+  CHECK_EQ(max_host, max_device);
+  std::cout << max_host << '\n';
+  // for (int i = 0; i < n-1; ++i)
+  //   std::cout << array[i] << ' ';
+  // std::cout << array[n-1] << '\n';
 
+  // int kernel_max_index = *max_index;
   cudaFree(device_array);
-  cudaFree(device_max_index);
+  //  cudaFree(device_max_index);
   free(array);
-  free(max_index);
+  // free(max_index);
 }
